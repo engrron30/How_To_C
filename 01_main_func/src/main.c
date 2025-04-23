1a48dbce58e1fe7fcc2d55e50f57d16d59f79821
engrron30 <ronaldtorresrosete30@gmail.com>
2025-04-24 00:36:04 +0800

    Starts the program with press F logic

--- 01_main_func/src/main.c
+++ 01_main_func/src/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SUCCESS 0
+#define FAILURE -1
+
+int press_F_to_next(void) {
+	char ch;
+
+	printf("----Press [F] to Continue----");
+	fflush(stdout);
+	ch = getchar();
+	
+	if (ch == 'F') {
+		printf("\r\033[K");
+		fflush(stdout);
+		return SUCCESS;
+	} else {
+		printf("Terminating the program...\n");
+		sleep(2);
+		printf("Thank you for playing!\n");
+		return FAILURE;
+	}
+}
 
 int main() {
+	system("head -n 1 main_func_narr.dat");
+	if (press_F_to_next() != SUCCESS)
+		return -1;
+	
+	char user_ans[1000];
+
 	return 0;
 
 }
